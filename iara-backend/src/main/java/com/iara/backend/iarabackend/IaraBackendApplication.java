5a2d1f831129c6307cd91c421a97a1d7245326c0
IgorDeMoraisPereiraDaSilva <igor.psilva@banctec.com>
2022-03-09 20:37:08 -0300

    feat:estrutura spring boot do iara

---
+++ iara-backend/src/main/java/com/iara/backend/iarabackend/IaraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iara.backend.iarabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaraBackendApplication.class, args);
+	}
+
+}
---
+++ iara-backend/src/test/java/com/iara/backend/iarabackend/IaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iara.backend.iarabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
