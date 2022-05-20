471a939dbeafaa19dd6c117693724d27898968ef
IgorDeMoraisPereiraDaSilva <igor.psilva@banctec.com>
2022-05-20 19:25:36 -0300

    test unitario - model/usuario

---
+++ iara/src/test/java/school/sptech/iara/model/UsuarioTest.java
@@ -0,0 +1,20 @@
+package school.sptech.iara.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @Test
+    void usuarioExiste() {
+    }
+
+    @Test
+    void autenticar() {
+    }
+
+    @Test
+    void logOff() {
+    }
+}
