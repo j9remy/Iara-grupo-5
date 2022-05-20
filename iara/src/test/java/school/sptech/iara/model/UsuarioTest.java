c0052e40ad3fa8160ec30a92728ae38942f126c2
IgorDeMoraisPereiraDaSilva <igor.psilva@banctec.com>
2022-05-20 19:28:39 -0300

    model/usuario teste unitario

--- iara/src/test/java/school/sptech/iara/model/UsuarioTest.java
+++ iara/src/test/java/school/sptech/iara/model/UsuarioTest.java
@@ -1,5 +1,7 @@
 package school.sptech.iara.model;
 
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +9,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class UsuarioTest {
 
     @Test
+    @DisplayName("Quando existir usuario com os dados informado")
     void usuarioExiste() {
+
+        //Arrange
+        Cliente user = new Cliente();
+        Prestador user2 = new Prestador();
+
+        //Act
+
+        //Assert
+        assertTrue(true);
     }
 
     @Test
     void autenticar() {
+        //Arrange
+        Cliente user = new Cliente();
+        Prestador user2 = new Prestador();
+
+        //Act
+
+        //Assert
+        assertTrue(true);
     }
 
     @Test
