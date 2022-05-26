14a39a1a6834d74f6ab76d1c03ec4ffed89750e5
j9remy <jeremy.freitas@bandtec.com.br>
2022-05-26 14:22:20 -0300

    Importaçõa txt - Prestador

--- iara/src/main/java/school/sptech/iara/controller/PrestadorController.java
+++ iara/src/main/java/school/sptech/iara/controller/PrestadorController.java
@@ -1,5 +1,6 @@
 package school.sptech.iara.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.sptech.iara.model.Endereco;
@@ -7,11 +8,20 @@ import school.sptech.iara.model.Prestador;
 import school.sptech.iara.repository.EnderecoRepository;
 import school.sptech.iara.repository.HabilidadeRepository;
 import school.sptech.iara.repository.PrestadorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import school.sptech.iara.repository.ServicoRepository;
-import school.sptech.iara.request.*;
+import school.sptech.iara.request.EnderecoSimplesRequest;
+import school.sptech.iara.request.PrestadorUpdateRequest;
+import school.sptech.iara.request.UsuarioEmailSenhaRequest;
+import school.sptech.iara.util.GravaArquivo;
 
 import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -136,8 +146,6 @@ public class PrestadorController {
         return ResponseEntity.status(400).build();
     }
 
-
-
     @GetMapping(value = "/foto/{idPrestador}", produces = "image/jpeg")
     public ResponseEntity<byte[]> getFoto(@PathVariable Integer idPrestador) {
         byte[] foto = repository.getFoto(idPrestador);
@@ -155,6 +163,147 @@ public class PrestadorController {
         }
         repository.atualizarFoto(idPrestador, novaFoto);
         return ResponseEntity.status(200).build();
+    }
+
+    @GetMapping("/registro/{nomeArq}")
+    public ResponseEntity postRegistro(@PathVariable String nomeArq) {
+        List<Prestador> lista = repository.findAll();
+        GravaArquivo gravaArq = new GravaArquivo();
+        int contaRegCorpo = 0;
+
+        // Monta o registro de header
+        String header = "PRESTADOR";
+        header += LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        header += "01";
+
+        // Grava o registro de header
+        gravaArq.gravaRegistro(header, nomeArq);
+
+        String corpo;
+        for (Prestador p : lista) {
+            corpo = "02";
+            corpo += String.format("%-5.5s", p.getNome());
+            corpo += String.format("%-5.5s", p.getSobrenome());
+            corpo += String.format("%-5.5s", p.getCpf());
+            corpo += String.format("%-5.5s", p.getDataNasc());
+            corpo += String.format("%-5.5s", p.getSexo());
+            corpo += String.format("%-5.5s", p.getEmail());
+            corpo += String.format("%-5.5s", p.getTelefone());
+            corpo += String.format("%-5.5s", p.getResumo());
+            corpo += String.format("%-5.5s", p.getAtendeDomicilio());
+            contaRegCorpo++;
+            gravaArq.gravaRegistro(corpo, nomeArq);
+        }
+
+        // Monta e grava o registro de trailer
+        String trailer = "01";
+        trailer += String.format("%010d", contaRegCorpo);
+        gravaArq.gravaRegistro(trailer, nomeArq);
+
+        return ResponseEntity.status(201).build();
+    }
+
+    @PostMapping("/registro/{nomeArq}")
+    public ResponseEntity getRegistro(String nomeArq) {
+        BufferedReader entrada = null;
+        String registro, tipoRegistro;
+        String nome, sobrenome, cpf, email, senha, telefone, resumo;
+        Timestamp dataNasc;
+        Character sexo;
+        Boolean atendeDomicilio;
+        int contaRegCorpoLido = 0;
+        int qtdRegCorpoGravado;
+
+        List<Prestador> listaLida = new ArrayList<>();
+
+        // try-catch para abrir o arquivo
+        try {
+            entrada = new BufferedReader(new FileReader(nomeArq));
+        } catch (IOException erro) {
+            System.out.println("Erro ao abrir o arquivo: " + erro);
+            return ResponseEntity.status(400).build();
+        }
+
+        try {
+            // Leitura do primeiro registro do arquivo
+            registro = entrada.readLine();
+
+            while (registro != null) { // enquanto não chegou ao final do arquivo
+                // obtém os 2 primeiros caracteres do registro
+                // 01234567
+                // 00NOTA20221
+                tipoRegistro = registro.substring(0,2);
+                if (tipoRegistro.equals("00")) {
+                    System.out.println("É um registro de header");
+                    System.out.println("Tipo de arquivo: " + registro.substring(2,6));
+                    System.out.println("Ano e semestre: " + registro.substring(6,11));
+                    System.out.println("Data e hora da gravação: " + registro.substring(11,30));
+                    System.out.println("Versão do documento: " + registro.substring(30,32));
+                }
+                else if (tipoRegistro.equals("01")) {
+                    System.out.println("É um registro de trailer");
+                    qtdRegCorpoGravado = Integer.parseInt(registro.substring(2,12));
+                    if (contaRegCorpoLido == qtdRegCorpoGravado) {
+                        System.out.println("Quantidade de registros lidos é compatível " +
+                                "com a quantidade de registros gravados");
+                    }
+                    else {
+                        System.out.println("Quantidade de registros lidos não é compatível " +
+                                "com a quantidade de registros gravados");
+                    }
+                }
+                else if (tipoRegistro.equals("00002")) {
+                    System.out.println("É um registro de corpo");
+                    nome = registro.substring(6,35).trim();
+                    sobrenome = registro.substring(36,85).trim();
+                    cpf = registro.substring(86,96).trim();
+                    dataNasc = Timestamp.valueOf(registro.substring(97,115).trim());
+                    sexo = registro.charAt(116);
+                    email = registro.substring(117,166).trim();
+                    senha = registro.substring(167,177).trim();
+                    telefone = registro.substring(178,191).trim();
+                    resumo = registro.substring(192,391).trim();
+                    atendeDomicilio = Boolean.valueOf(registro.substring(392,393).trim());
+                    contaRegCorpoLido++;
+
+                    Prestador p = new Prestador(nome,
+                            sobrenome,
+                            cpf,
+                            dataNasc,
+                            email,
+                            senha,
+                            sexo,
+                            telefone,
+                            resumo,
+                            atendeDomicilio);
+
+                    // No projeto de PI, poderia fazer:
+                    // repository.save(a);
+
+                    // No nosso caso, vamos adicionar o objeto a na listaLida:
+                    repository.save(p);
+                }
+                else {
+                    System.out.println("Tipo de registro inválido!");
+                }
+
+                // Lê o próximo registro
+                registro = entrada.readLine();
+            }
+
+            entrada.close();
+        }
+        catch (IOException erro) {
+            System.out.println("Erro ao ler o arquivo: " + erro);
+            return ResponseEntity.status(400).build();
+        }
+
+        // Vamos exibir a listaLida
+        System.out.println("\nConteúdo da lista lida:");
+        for (Prestador p : listaLida) {
+            System.out.println(p);
+        }
+        return ResponseEntity.status(201).build();
     }
 
     @GetMapping("/relatorio")
--- iara/src/main/java/school/sptech/iara/request/RegistroRequest.java
+++ iara/src/main/java/school/sptech/iara/request/RegistroRequest.java
@@ -1,13 +1,14 @@
 package school.sptech.iara.request;
 
 import school.sptech.iara.model.Cliente;
-
+import school.sptech.iara.model.Prestador;
 import java.util.List;
 
 public class RegistroRequest {
 
     private String nomeArq;
-    private List<Cliente> lista;
+    private List<Cliente> listaCliente;
+    private List<Prestador> listaPrestador;
 
     public String getNomeArq() {
         return nomeArq;
@@ -17,11 +18,19 @@ public class RegistroRequest {
         this.nomeArq = nomeArq;
     }
 
-    public List<Cliente> getLista() {
-        return lista;
+    public List<Cliente> getListaCliente() {
+        return listaCliente;
     }
 
-    public void setLista(List<Cliente> lista) {
-        this.lista = lista;
+    public void setListaCliente(List<Cliente> listaCliente) {
+        this.listaCliente = listaCliente;
+    }
+
+    public List<Prestador> getListaPrestador() {
+        return listaPrestador;
+    }
+
+    public void setListaPrestador(List<Prestador> listaPrestador) {
+        this.listaPrestador = listaPrestador;
     }
 }
