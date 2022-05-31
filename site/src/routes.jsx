fb01b10856a0ad0a26697efc1129ca6842ac9555
ycxs12 <yancruzsantos97@gmail.com>
2022-05-31 20:56:47 -0300

    correção da Routes.jsx

--- site/src/routes.jsx
+++ site/src/routes.jsx
@@ -7,14 +7,9 @@ function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
-<<<<<<< HEAD
                 <Route path="/escolhaCadastro" element={<EscolhaCadastro/>}/>
                 <Route path="/cadastroCliente" element={<CadastroCliente/>}/>
                 <Route path="/login" element={<Login/>}/>
-=======
-                <Route path="/escolha-cadastro" element={<EscolhaCadastro/>}/>
-                <Route path="/cadastro-cliente" element={<CadastroCliente/>}/>
->>>>>>> 337139a416ce724edd27fa76e468d4f1e4abf6b3
             </Routes>
         </BrowserRouter>
 
