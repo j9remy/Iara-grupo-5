30fd5f369d68d529145174e614575710b7d91d16
j9remy <j9remy@outlook.com>
2022-09-01 21:41:44 -0300

    Alterações Sliders - React

--- site/src/components/CarrocelCategorias.jsx
+++ site/src/components/CarrocelCategorias.jsx
@@ -22,7 +22,7 @@ function SamplePrevArrow(props) {
     );
 }
 
-function CarrocelCategorias() {
+function CarrocelCategorias(props) {
 
     const settings = {
         infinite: true,
@@ -35,45 +35,59 @@ function CarrocelCategorias() {
         <div id="categorias" class="categorias card bg-red">
             <Slider {...settings}>
                 <div>
-                    <span class="item">
-                        <img src="../img/categorias/img-corte.png" alt="Corte de cabelo" />
-                        <span>Corte de Cabelo</span>
+                    <span class="item" onClick={() => props.funcaoCategoria(1)}>
+                        <a href="#">
+                            <img src="../img/categorias/img-corte.png" alt="Corte de cabelo" />
+                            <span>Corte de Cabelo</span>
+                        </a>
+                    </span>
+                </div>
+                <div>
+                    <span class="item" onClick={() => props.funcaoCategoria(2)}>
+                        <a href="#">
+                            <img src="../img/categorias/img-hidratacao.png" alt="Hidratação" />
+                            <span>Hidratação</span>
+                        </a>
                     </span>
                 </div>
                 <div>
                     <span class="item">
-                        <img src="../img/categorias/img-hidratacao.png" alt="Hidratação" />
-                        <span>Hidratação</span>
+                        <a href="">
+                            <img src="../img/categorias/img-maquiagem.png" alt="Maquiagem" />
+                            <span>Maquiagem</span>
+                        </a>
                     </span>
                 </div>
                 <div>
                     <span class="item">
-                        <img src="../img/categorias/img-maquiagem.png" alt="Maquiagem" />
-                        <span>Maquiagem</span>
+                        <a href="">
+                            <img src="../img/categorias/img-manicure.png" alt="Manicure" />
+                            <span>Manicure</span>
+                        </a>
                     </span>
                 </div>
                 <div>
                     <span class="item">
-                        <img src="../img/categorias/img-manicure.png" alt="Manicure" />
-                        <span>Manicure</span>
+                        <a href="">
+                            <img src="../img/categorias/img-designer-sobrancelhas.png" alt="Design de sobrancelhas" />
+                            <span>Design de sobrancelhas</span>
+                        </a>
                     </span>
                 </div>
                 <div>
                     <span class="item">
-                        <img src="../img/categorias/img-designer-sobrancelhas.png" alt="Design de sobrancelhas" />
-                        <span>Design de sobrancelhas</span>
+                        <a href="">
+                            <img src="../img/categorias/img-massagem.png" alt="Massagem" />
+                            <span>Massagem</span>
+                        </a>
                     </span>
                 </div>
                 <div>
                     <span class="item">
-                        <img src="../img/categorias/img-massagem.png" alt="Massagem" />
-                        <span>Massagem</span>
-                    </span>
-                </div>
-                <div>
-                    <span class="item">
+                        <a href="">
                         <img src="../img/categorias/img-pedicure.png" alt="Pedicure" />
-                        <span>Pedicure</span>
+                            <span>Pedicure</span>
+                        </a>
                     </span>
                 </div>
             </Slider>
--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,22 +1,121 @@
+import { useState, useEffect } from "react";
 import CarrocelCategorias from "../components/CarrocelCategorias";
 import CarrocelProfissionais from "../components/CarrocelProfissionais";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
+import Slider from "react-slick";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "absolute", color: "#de0235" }}
+      onClick={onClick}
+    />
+  );
+}
+
 
 function Home() {
-    return (
-        <>
-            <main class="margin-top-thirty">
-                <div class="container">
-                    <Header />
-                    <CarrocelCategorias />
-                    <CarrocelProfissionais />
-                    <Footer/>
-                </div>
-            </main>
-        </>
-    )
+  const [profissionais, setProfissionais] = useState([]);
 
+  const settings = {
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+
+  function selecionarCategoria(idCategoria) {
+    //REQUISIÇÃO BACKEND
+
+    if (idCategoria === 1) {
+        
+        setProfissionais([
+            {
+              nome: "Ana clara",
+              distancia: "6,4 KM",
+              habilidade: "Cacheado",
+              avaliacao: "4",
+              foto: "../img/profissionais/img-prof-1.png",
+            },
+            {
+                nome: "Diogo",
+                distancia: "7,4 KM",
+                habilidade: "Liso",
+                avaliacao: "4",
+                foto: "../img/profissionais/img-prof-1.png",
+            },
+            {
+                nome: "Ana clara",
+                distancia: "6,4 KM",
+                habilidade: "Cacheado",
+                avaliacao: "4",
+                foto: "../img/profissionais/img-prof-1.png",
+              },
+              {
+                  nome: "Diogo",
+                  distancia: "7,4 KM",
+                  habilidade: "Liso",
+                  avaliacao: "4",
+                  foto: "../img/profissionais/img-prof-1.png",
+              }
+          ]);
+    } else {
+        setProfissionais([
+            {
+              nome: "Diogo",
+              distancia: "7,4 KM",
+              habilidade: "Liso",
+              avaliacao: "4",
+              foto: "../img/profissionais/img-prof-1.png",
+            },
+          ]);
+    }
+
+  }
+
+  return (
+    <>
+      <main class="margin-top-thirty">
+        <div class="container">
+          <Header />
+
+          <CarrocelCategorias funcaoCategoria={selecionarCategoria} />
+
+          <div id="profissionais" class="profissionais">
+            <Slider {...settings}>
+              {profissionais.map((profissional) => (
+                <CarrocelProfissionais
+                  nome={profissional.nome}
+                  habilidade={profissional.habilidade}
+                  distancia={profissional.distancia}
+                  avaliacao={profissional.avaliacao}
+                  foto={profissional.foto}
+                />
+              ))}
+            </Slider>
+          </div>
+
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
 }
 export default Home;
