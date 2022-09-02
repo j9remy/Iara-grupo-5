6e03696f22cb2e43387d3f661d7f84013acf1195
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2022-09-01 21:29:15 -0300

    fix: correção associação de habilidade para prestador

--- iara/src/main/java/school/sptech/iara/repository/CategoriaRepository.java
+++ iara/src/main/java/school/sptech/iara/repository/CategoriaRepository.java
@@ -3,5 +3,8 @@ package school.sptech.iara.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.sptech.iara.model.Categoria;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    Optional<Categoria> findByCategoria(String categoria);
 }
--- iara/src/main/java/school/sptech/iara/repository/HabilidadeRepository.java
+++ iara/src/main/java/school/sptech/iara/repository/HabilidadeRepository.java
@@ -4,9 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import school.sptech.iara.model.Categoria;
 import school.sptech.iara.model.Habilidade;
 
+import java.util.Optional;
+
 public interface HabilidadeRepository extends JpaRepository<Habilidade, Integer> {
 
     boolean existsByDescricao(String desc);
-    boolean existsByCategoria(Categoria hab);
+    boolean existsByCategoria(Categoria cat);
+    boolean existsByCategoriaAndDescricao(Categoria categoria, String descricao);
+    Optional<Habilidade> findByCategoriaAndDescricao(Categoria categoria, String descricao);
+
+
 //    Boolean existsByHabilidadeAndDescricao(String habilidade, String descricao);
 }
