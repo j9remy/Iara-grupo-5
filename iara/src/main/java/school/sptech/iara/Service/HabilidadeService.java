56cfa1f2f0661e2c11b4080353db2e1ac3c1141e
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2022-09-03 17:27:27 -0300

    fix: desassociar habilidade de prestador

--- iara/src/main/java/school/sptech/iara/Service/HabilidadeService.java
+++ iara/src/main/java/school/sptech/iara/Service/HabilidadeService.java
@@ -1,6 +1,7 @@
 package school.sptech.iara.Service;
 
 import com.github.javafaker.Cat;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,24 @@ public class HabilidadeService {
         return ResponseEntity.status(204).build();
     }
 
+    public ResponseEntity<Habilidade> buscaHabilidade(Integer idHabilidade){
+        Optional<Habilidade> habilidadeOptional = habilidadeRepository.findById(idHabilidade);
+        if (habilidadeOptional.isPresent()){
+            Habilidade habilidade = habilidadeOptional.get();
+            return ResponseEntity.status(200).body(habilidade);
+        }
+        return ResponseEntity.status(404).build();
+    }
+
+    public ResponseEntity<Habilidade> buscaHabilidade(@NotNull Categoria categoria, @NotNull String descricao){
+        Optional<Habilidade> habilidadeOptional = habilidadeRepository.findByCategoriaAndDescricao(categoria,descricao);
+        if (habilidadeOptional.isPresent()){
+            Habilidade habilidade = habilidadeOptional.get();
+            return ResponseEntity.status(200).body(habilidade);
+        }
+        return ResponseEntity.status(404).build();
+    }
+
     public ResponseEntity<Void> associarPrestador(PrestadorHabilidadeRequest req){
         Optional<Prestador> prestadorOptional = prestadorRepository.findById(req.getIdPrestador());
         if (prestadorOptional.isPresent()){
@@ -47,24 +66,45 @@ public class HabilidadeService {
                     return ResponseEntity.status(400).build();
                 categoria = categoriaResponseEntity.getBody();
             }
-            if (!prestador.habilidadeExiste(new Habilidade(categoria, req.getDescricao()))){
-                if (habilidadeRepository.existsByCategoriaAndDescricao(categoria, req.getDescricao())){
-                    Optional<Habilidade> habilidadeOptional = habilidadeRepository.findByCategoriaAndDescricao(categoria, req.getDescricao());
-                    if (habilidadeOptional.isPresent()){
-                        Habilidade habilidade = habilidadeOptional.get();
-                        prestador.addHabilidade(habilidade);
-                        prestadorRepository.save(prestador);
-                        return ResponseEntity.status(201).build();
-                    }
-                    return ResponseEntity.status(400).build();
+            Optional<Habilidade> habilidadeOptional = habilidadeRepository.findByCategoriaAndDescricao(categoria, req.getDescricao());
+            if (habilidadeOptional.isPresent()){
+                if (!prestador.habilidadeExiste(new Habilidade(categoria, req.getDescricao()))) {
+                    Habilidade habilidade = habilidadeOptional.get();
+                    prestador.addHabilidade(habilidade);
+                    habilidadeRepository.save(habilidade);
+                    prestadorRepository.save(prestador);
+                    return ResponseEntity.status(201).build();
                 }
-                Habilidade habilidade = new Habilidade(categoria, req.getDescricao());
-                habilidadeRepository.save(habilidade);
-                prestador.addHabilidade(habilidade);
-                prestadorRepository.save(prestador);
-                return ResponseEntity.status(201).build();
+                return ResponseEntity.status(400).build();
             }
+            Habilidade habilidade = new Habilidade(categoria, req.getDescricao());
+            habilidadeRepository.save(habilidade);
+            prestador.addHabilidade(habilidade);
+            prestadorRepository.save(prestador);
+            return ResponseEntity.status(201).build();
         }
         return ResponseEntity.status(400).build();
+    }
+
+    public ResponseEntity<Void> desassociarPrestador(PrestadorHabilidadeRequest req){
+        Optional<Prestador> prestadorOptional = prestadorRepository.findById(req.getIdPrestador());
+        if (prestadorOptional.isPresent()) {
+            Prestador prestador = prestadorOptional.get();
+            ResponseEntity<Categoria> categoriaResponseEntity = categoriaService.buscaCategoria(req.getCategoria());
+            if (categoriaResponseEntity.getStatusCodeValue() == 200){
+                Categoria categoria = categoriaResponseEntity.getBody();
+                ResponseEntity<Habilidade> habilidadeResponseEntity = buscaHabilidade(categoria, req.getDescricao());
+                if (habilidadeResponseEntity.getStatusCodeValue() == 200){
+                    Habilidade habilidade = habilidadeResponseEntity.getBody();
+                    if (prestador.habilidadeExiste(habilidade)) {
+                        prestador.removeHabilidade(habilidade);
+                        prestadorRepository.save(prestador);
+                        return ResponseEntity.status(202).build();
+                    }
+                }
+            }
+            return ResponseEntity.status(400).build();
+        }
+        return ResponseEntity.status(404).build();
     }
 }
