66ebaa9604543df199437fb98e6fba645628d6a6
Amanda Caramico <79462423+acaramico@users.noreply.github.com>
2022-10-06 19:25:59 -0300

    Update CarrosselPropaganda.jsx

--- site/src/components/CarrosselPropaganda.jsx
+++ site/src/components/CarrosselPropaganda.jsx
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
-import banner from "../html-css-template/img/banner/Banner-Iara.png";
-import banner2 from "../html-css-template/img/banner/Banner-Iara2.png";
-import banner3 from "../html-css-template/img/banner/Banner-Iara3.png";
+import banner from "../html-css-template/img/banner/Banner-Iara - Copia.png";
+import banner2 from "../html-css-template/img/banner/Banner-Iara2 - Copia.png";
+import banner3 from "../html-css-template/img/banner/Banner-Iara3 - Copia.png";
 import { GoChevronRight } from "react-icons/go";
 import { GoChevronLeft } from "react-icons/go";
 
@@ -13,21 +13,21 @@ function CarrosselPropaganda() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        nextArrow: <GoChevronRight color="white" />,
-        prevArrow: <GoChevronLeft  color="white"/>
+        nextArrow: <GoChevronRight color="rgb(222 2 53)" />,
+        prevArrow: <GoChevronLeft  color="rgb(222 2 53)"/>
     };
 
     return (
-        <div class="categorias item">
+        <div class="banner">
             <Slider {...settings}>
                 <div class="img">
-                    <img src={banner} />
+                    <img src={banner} class="radius"/>
                 </div>
                 <div class="img">
-                    <img src={banner2} />
+                    <img src={banner2} class="radius" />
                 </div>
                 <div class="img">
-                    <img src={banner3} />
+                    <img src={banner3} class="radius" />
                 </div>
             </Slider>
         </div>
